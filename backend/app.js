1eff9c3c9ce56aacd4b5a21e5fbcd62492221732
Richard Kubina <rjkubina@gmail.com>
2022-10-16 09:29:05 -0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
